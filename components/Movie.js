3494e5269989f621bcee82317fca771024ee6dfc
Soumyajit Pathak <s.pathak5995@gmail.com>
2018-07-03 02:35:10 +0530

    route prefetching

---
+++ components/Movie.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Router from 'next/router';
+import Link from 'next/link';
+
+import { getImageSrc } from '../utils/apiCalls';
+
+class Movie extends Component {
+  prefetchMoviePage = () => {
+    Router.prefetch(`/movie/${this.props.id}`);
+  };
+
+  render() {
+    const { id, poster_path, title, overview } = this.props;
+    return (
+      <Link as={`/movie/${id}`} href={`/movie?id=${id}`} key={id}>
+        <div className="card" key={id} onMouseOver={this.prefetchMoviePage}>
+          <div className="card-image">
+            <img src={getImageSrc(poster_path, 342)} className="img-responsive" />
+          </div>
+          <div className="card-header">
+            <div className="card-title h5 text-primary text-ellipsis">{title}</div>
+          </div>
+          <div className="card-body text-ellipsis">{overview}</div>
+        </div>
+      </Link>
+    );
+  }
+}
+
+export default Movie;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,7 @@
-import Link from 'next/link';
+import Movie from '../components/Movie';
+import { getUpcomingMovies } from '../utils/apiCalls';
 
-import { getUpcomingMovies, getImageSrc } from '../utils/apiCalls';
-
-const Home = ({ movies }) => (
-  <div className="home">
-    {movies.map(({ title, id, poster_path, overview }) => (
-      <Link as={`/movie/${id}`} href={`/movie?id=${id}`} key={id}>
-        <div className="card" key={id}>
-          <div className="card-image">
-            <img src={getImageSrc(poster_path, 342)} className="img-responsive" />
-          </div>
-          <div className="card-header">
-            <div className="card-title h5 text-primary text-ellipsis">{title}</div>
-          </div>
-          <div className="card-body text-ellipsis">{overview}</div>
-        </div>
-      </Link>
-    ))}
-  </div>
-);
+const Home = ({ movies }) => <div className="home">{movies.map(props => <Movie {...props} key={props.id} />)}</div>;
 
 Home.getInitialProps = async () => {
   const res = await getUpcomingMovies();
