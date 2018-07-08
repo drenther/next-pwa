2442d3876d892d9721a16f13869671da60d679bf
Soumyajit Pathak <s.pathak5995@gmail.com>
2018-07-09 00:17:36 +0530

    loading indicator added

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Router from 'next/router';
 
 import Head from './Head';
 import Nav from './Nav';
 
-const Header = props => (
-  <div className="header">
-    <Head title="Movies" />
-    <Nav />
-  </div>
-);
+class Header extends Component {
+  state = { loading: false };
+
+  componentDidMount() {
+    Router.onRouteChangeStart = () => {
+      this.setState({ loading: true });
+    };
+    Router.onRouteChangeComplete = () => {
+      this.setState({ loading: false });
+    };
+    Router.onRouteChangeError = () => {
+      this.setState({ loading: false });
+    };
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <Loader loading={this.state.loading} />
+        <Head title="Movies" />
+        <Nav />
+      </div>
+    );
+  }
+}
+
+const Loader = ({ loading }) => <div className={loading ? 'loading-show' : ''} id="loader-bar" />;
 
 export default Header;
