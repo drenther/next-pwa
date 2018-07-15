b3bf5286971c2b63ad178b64f624d2d9b15f9a50
Soumyajit Pathak <s.pathak5995@gmail.com>
2018-07-15 23:51:14 +0530

    remove unnecessary div wrapper

--- components/Oops.js
+++ components/Oops.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const Oops = () => (
-  <div className="movie">
-    <div className="card">
-      <div className="card-header">
-        <div className="card-title h3 text-gray">
-          Oops! Looks like you're offline and this page is not cached either. Try again when you're online.
-        </div>
+  <div className="card">
+    <div className="card-header">
+      <div className="card-title h3 text-gray">
+        Oops! Looks like you're offline and this page is not cached either. Try again when you're online.
       </div>
     </div>
   </div>
